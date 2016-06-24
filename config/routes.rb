3b8e4863bd09a7fc8d94477ab949b8ebb1756c01
Tim McMackin <timothymcmackin@yahoo.com>
2016-06-24 16:44:26 -0400

    home and about nav links

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,8 @@ Rails.application.routes.draw do
 
   get 'about/index'
 
+  get 'about' => 'about#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
