e8ea8139efe8e00ebcce2cb3af8b4e1262dc9734
Tim McMackin <timothymcmackin@yahoo.com>
2016-06-29 13:14:32 -0400

    books controller and new books form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
   def new
+    @page_title = 'Add book'
+    @book = Book.new
+    @category = Category.new
+    @author = Author.new
+    @publisher = Publisher.new
   end
 
   def create
